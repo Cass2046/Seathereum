93940a59d891ecf08ed6c2b129076b063adebf49
lu.yin2046 <lu.yin2046@gmail.com>
2018-11-16 01:39:48 -0600

    load in p5
    load in p5 framework, object shows in page

--- game/p5/empty-example/sketch.js
+++ game/p5/empty-example/sketch.js
@@ -1,39 +1,63 @@
 //P5 JS framework 
-var creature;
-var pipes = [];
+//let creature;
+//var pipes = [];
+// var color;
+
 function setup() {
-    createCancas(400, 600);
-    creature = new creature();
-    pipes.push(new pipes());
+    createCanvas(600, 400);
+    //creature = new creature(200, 200, 40);
+    //pipes.push(new pipes());
 }
 
 function draw() {
-    background(0);
+    // color=map(creature.x,0,600,255)
+    // background(color);
+    background(250, 250, 100);
+    fill(250, 200, 200);
+    ellipse(60, 120, 20, 40);
+    // for (var i =pipes.length-1; i>=0; i--){
+    //     pipes[i].show();
+    //     pipes[i].update();
+    //     if (pipe[i].hits(creature)) {
+    //         console.log("HIT");
+    //     }
+    //     if (pipe[i].offcsreen()) {
+    //         pipes.splice(i, 1);
+    //     }
+    // }
 
-    for (var i =pipes.length-1; i>=0; i--){
-        pipes[i].show();
-        pipes[i].update();
-        if (pipe[i].hits(creature)) {
-            console.log("HIT");
-        }
-        if (pipe[i].offcsreen()) {
-            pipes.splice(i, 1);
-        }
-    }
+    //creature.update();
+    // creature.move();
+    // creature.show();
 
-    creature.update();
-    creature.show();
+    // if (frameCount %70 == 0){
+    //     pipes.push(new pipes());
+    // }
 
-    if (frameCount %70 == 0){
-        pipes.push(new pipes());
-    }
-
-    
 }
+// class creature {
+//     constructor (x, y, v){
+//         this.x = x;
+//         this.y = y;
+//         this.v = v;
+//     }
 
-function keyPressed() {
-    if (key == ' ' ) {
-        creature.up();
-        //console.log("1");
-    }
-}
+//     move(){
+//         this.x = this.x + random(-15, 15);
+//         this.y = this.y + random(-15, 15);
+//     }
+
+//     show(){
+//         stroke(255);
+//         strokeWeight(5);
+//         fill(255, 0, 100, 50);
+//         ellipse(this.x, this.y, this.v*2);
+//     }
+// }
+
+// function keyPressed() {
+//     if (key == ' ' ) {
+//         creature.up();
+//         //console.log("1");
+//     }
+// }
