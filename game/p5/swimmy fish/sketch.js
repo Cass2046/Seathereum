21d1cc53343724586af477a30f2ba3451951eb6d
lu.yin2046 <lu.yin2046@gmail.com>
2018-11-25 23:55:35 -0600

    a creature escape from an open month

--- game/p5/swimmy fish/sketch.js
+++ game/p5/swimmy fish/sketch.js
@@ -1,11 +1,6 @@
 var creature={
-    x:50,
-    y:120,
-    // this.y = height/2,
-    // this.x = 50,
-    // this.gravity = 0.7,
-    // this.lift =-4,
-    // this.velocity = 0,
+    x:100,
+    y:460,
 };
 
 var coral={
@@ -17,6 +12,16 @@ var coral={
     length2: 100,
 };
 
+var upteeth={
+    x:20,
+    y:290,
+};
+
+var downteeth={
+    x:120,
+    y:565,
+};
+
 function setup() {
     createCanvas(800, 600);
     //creature = new creature(200, 200, 40);
@@ -25,70 +30,46 @@ function setup() {
 
 function draw() {
     // background color tutorial 1-7 map func
-    colorR=map(creature.x, 0, 600, 0, 200);
-    background(colorR/2, 120, 180);
+    // colorR=map(creature.x, 0, 600, 0, 200);
+    // background(colorR/2, 120, 200);
+    background(150, 190, 190);
+    stroke(255);
+    strokeWeight(3);
+    fill(40, 120, 200);
+    triangle(0, 0, 400, 0, 0, 270);
+    triangle(400, 0, 800, 0, 800, 270);
+
+    //mouth
+    fill(200, 30, 50);
+    rect(upteeth.x-3, upteeth.y-3, 760, 280);
     
-    //colorG=map(creature.y, );
-    fill(180, 20, 90);
+    //teeth
+    fill(255);
+    //upteeth
+    triangle(upteeth.x, upteeth.y, upteeth.x+40, upteeth.y, upteeth.x+12, upteeth.y+80);
+    triangle(upteeth.x+50, upteeth.y, upteeth.x+90, upteeth.y, upteeth.x+62, upteeth.y+90);
+    triangle(upteeth.x+290, upteeth.y, upteeth.x+340, upteeth.y, upteeth.x+320, upteeth.y+90);
+    //downteeth
+    triangle(downteeth.x, downteeth.y, downteeth.x+40, downteeth.y, downteeth.x+12, downteeth.y-80);
+    triangle(downteeth.x+100, downteeth.y, downteeth.x+160, downteeth.y, downteeth.x+140, downteeth.y-120);
+    //upteeth.x=upteeth.x+100;
+    
+    //creature
+    fill(110, 200, 90);
     ellipse(creature.x, creature.y, 35, 40);
 
     if (mouseIsPressed){
        creature.y=creature.y-9;
     }
 
-    fill (100,200,100);
-    rect(coral.x,coral.y1,coral.width,coral.length1);
+    // fill (190,100,200);
+    // triangle(100, -1, 160, -1, 120, 270);
+    //triangle(200, 800, 260, 800, 220, 370);
+    //rect(coral.x,coral.y1,coral.width,coral.length1);
     //rect(coral.x,-2,coral.y2,coral.width, coral.length2);
 
-    // creature.x=creature.x+3;
-     creature.y=creature.y+2.7;
+    creature.x=creature.x+1;
+    creature.y=creature.y+1;
 
     coral.x=coral.x-1;
-
-   
-    // for (var i =pipes.length-1; i>=0; i--){
-    //     pipes[i].show();
-    //     pipes[i].update();
-    //     if (pipe[i].hits(creature)) {
-    //         console.log("HIT");
-    //     }
-    //     if (pipe[i].offcsreen()) {
-    //         pipes.splice(i, 1);
-    //     }
-    // }
-
-    //creature.update();
-    // creature.move();
-    // creature.show();
-
-    // if (frameCount %70 == 0){
-    //     pipes.push(new pipes());
-    // }
-
 }
-// class creature {
-//     constructor (x, y, v){
-//         this.x = x;
-//         this.y = y;
-//         this.v = v;
-//     }
-
-//     move(){
-//         this.x = this.x + random(-15, 15);
-//         this.y = this.y + random(-15, 15);
-//     }
-
-//     show(){
-//         stroke(255);
-//         strokeWeight(5);
-//         fill(255, 0, 100, 50);
-//         ellipse(this.x, this.y, this.v*2);
-//     }
-// }
-
-// function keyPressed() {
-//     if (key == ' ' ) {
-//         creature.up();
-//         //console.log("1");
-//     }
-// }
