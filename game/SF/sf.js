10eedfa58222817656d234ffbb2e39a2c379a885
lu.yin2046 <lu.yin2046@gmail.com>
2019-02-10 19:55:44 -0600

    random push barriers(plant)
    5 types of north plant and 5 types of south plant pushed  ramdomly

--- game/SF/sf.js
+++ game/SF/sf.js
@@ -8,34 +8,36 @@ cvs.style = "position:absolute; left: 20%";
 var creature = new Image();
 var bg = new Image();
 var plantNorth = new Image();
-var plantSouth = new Image();
 
-var grass = new Image();
-var vine = new Image();
-var ubrown = new Image();
-var clover = new Image();
-var redflower = new Image();
-var leave = new Image();
-var upa = new Image();
-var upb = new Image();
-var upc = new Image();
-var upd = new Image();
+var up1 = new Image();
+var up2 = new Image();
+var up3 = new Image();
+var up4 = new Image();
+var up5 = new Image();
+
+var down1 = new Image();
+var down2 = new Image();
+var down3 = new Image();
+var down4 = new Image();
+var down5 = new Image();
+
 
 //source path
 creature.src = "pic/gost.gif";
 bg.src = "pic/bg.jpeg";
 plantNorth.src = "pic/upplant.png";
-plantSouth.src = "pic/downplant.png";
-grass.src = "pic/grass.png";
-vine.scr = "pic/vine.png";
-ubrown = "pic/upbrown.png";
-clover = "pic/clover.png";
-redflower = "pic/redflower.png";
-leave = "pic/leave.png";
-upa = "pic/upa.png";
-upb = "pic/upa.png";
-upc = "pic/upa.png";
-upd = "pic/upa.png";
+
+up1.src = "pic/up1.png";
+up2.src = "pic/up2.png";
+up3.src = "pic/up3.png";
+up4.src = "pic/up4.png";
+up5.src = "pic/up4.png";
+
+down1.src = "pic/down1.png";
+down2.src = "pic/down2.png";
+down3.src = "pic/down3.png";
+down4.src = "pic/down4.png";
+down5.src = "pic/down5.png";
 
 var gap = 90;
 var constant;
@@ -47,11 +49,11 @@ var score = 0;
 /* 
 /  load audio
 */
-var jp = new Audio();
-var scor = new Audio();
+// var jp = new Audio();
+// var scor = new Audio();
 
 //audio source
-scor.src = "bgm/score.mp3";
+//scor.src = "bgm/score.mp3";
 
 
 /*
@@ -65,14 +67,20 @@ function moveUp(){
 }
 
 /*
-* barriers function and coordinates
-*/ 
-var barr = [leave, upa, upb, upc, upd, vine, ubrown];
+* North Plant function and coordinates
+*/
+
+var up_barr = [up1, up2, up3, up4, up5];
 //random pick a barrier from barr array
-barr [Math.floor(Math.random() * barr.length)] = {
-    x : Math.floor(Math.random() * cvs.width * 200),
-    y : Math.floor(Math.random() * cvs.height * 100)
-};
+var up_plant = up_barr[Math.floor(Math.random() * 5)];
+
+/*
+* South Plant function and coordinates
+*/ 
+
+var down_barr = [down1, down2, down3, down4, down5];
+//random pick a barrier from barr array
+var down_plant = down_barr[Math.floor(Math.random() * 5)];
 
 // function drawBarr(){
 //     for (var i = 0; i < 750; i++){
@@ -92,7 +100,7 @@ barr [Math.floor(Math.random() * barr.length)] = {
 
 var plant = [];
 plant[0] = {
-    x : cvs.width,
+    x : cvs.width - 200,
     y : 0
 };
 
@@ -106,32 +114,31 @@ function draw(){
     //plant
     for(var i = 0; i < plant.length; i++){
         constant = plantNorth.height + gap;
-        sf.drawImage(plantNorth,plant[i].x,plant[i].y);
-        sf.drawImage(plantSouth,plant[i].x,plant[i].y + constant);
-        
+        //sf.drawImage(plantNorth,plant[i].x,plant[i].y);
+        //sf.drawImage(plantSouth,plant[i].x,plant[i].y + constant);
+        sf.drawImage(down_plant, plant[i].x,plant[i].y + constant-30);
+        sf.drawImage(up_plant, plant[i].x + 90, plant[i].y);
 
         console.log(plant[i].x);
-            // console.log(plant[i].height);
-            // console.log(plant[i].width);
              
         plant[i].x--;
         
-        if(plant[i].x == 90){
+        if(plant[i].x == 290){
             //push new plant with random function
-            plant.push({
+            up_plant.push({
                 x : cvs.width - Math.floor(Math.random() *  10),
                 y : Math.floor(Math.random() * 100) - Math.floor(Math.random() * 50)
             });
         }
 
         if(plant[i].x == 100){
-            barr.push({
+            down_plant.push({
                 x : Math.floor(Math.random() * 600),
                 y : Math.floor(Math.random() * 200) + Math.floor(Math.random() * 50)
             });
         }
         /*
-        * Game over rulea
+        * Game over rule
         */
         
         //hit the plant
