57d17e7068e85337d4aba32f93e483ad9fe3ea52
lu.yin2046 <lu.yin2046@gmail.com>
2019-02-25 14:42:31 -0600

    menu
    bug fixed

--- game/SF/sf.js
+++ game/SF/sf.js
@@ -20,37 +20,36 @@ var down3 = new Image();
 var down4 = new Image();
 var down5 = new Image();
 
-var grass = new Image();
-var root = new Image();
-var leave = new Image();
+var shark = new Image();
+var shell = new Image();
 
 
 //source path
 creature.src = "pic/gost.gif";
 bg.src = "pic/bg.jpeg";
+shark.src = "pic/shark.png";
+shell.scr = "pic/shell/png"; //102
 
-up1.src = "pic/up1.png";
-up2.src = "pic/up2.png";
-up3.src = "pic/up3.png";
-up4.src = "pic/up4.png";
-up5.src = "pic/up4.png";
+up1.src = "pic/up1.png"; //w91 h311
+up2.src = "pic/up2.png"; //52, 170
+up3.src = "pic/up3.png"; //84, 270
+up4.src = "pic/up4.png"; //93, 311
 
-down1.src = "pic/down1.png";
-down2.src = "pic/down2.png";
-down3.src = "pic/down3.png";
-down4.src = "pic/down4.png";
-down5.src = "pic/down5.png";
+down1.src = "pic/down1.png"; //w138 h180
+down2.src = "pic/down2.png"; //123, 198
+down3.src = "pic/down3.png"; //135, 199
+down4.src = "pic/down4.png"; //121, 199
+down5.src = "pic/down5.png"; //140, 205
 
-grass.src = "pic/grass.png";
-root.src = "pic/root.png";
-leave.src = "pic/leave.png";
-
-var gap = 100;
+var gap = 140;
 var constant;
 var CX = 80;
 var CY = 120;
+// var SX = ;
+// var SY = ;
 var gravity = 1.2;
 var score = 0;
+var s = 60;
 
 /* 
 /  load audio
@@ -73,7 +72,7 @@ function moveUp(){
 /*
 * North Plant function and coordinates
 */
-var up_barr = [up4, up2, up3, up1, up5, up2, up2, up3, up4, up1];
+var up_barr = [up1, up2, up3, up4, up4, up1, up2, up3, up4, up1];
 //random pick a barrier from barr array
 var up_plant = up_barr[Math.floor(Math.random() * 10)];
 var up_pos = [];
@@ -86,7 +85,8 @@ up_pos[0] = {
 /*
 * South Plant function and coordinates
 */ 
-var down_barr = [down1, down2, down3, down4, down5, grass, root, leave, grass, down2];
+var down_barr = [down1, down2, down3, down4, down5, down1, down2, down3, down4, down5];
+// var down_barr = [down3, down3, down3, down5, down5, down5, down5, down5, down5, down5];
 //random pick a barrier from barr array
 var down_plant = down_barr[Math.floor(Math.random() * 10)];
 var down_pos = [];
@@ -94,93 +94,83 @@ down_pos[0] = {
     x : cvs.width - 160
 };
 
-var plant = [];
-plant[0] = {
+var upplant = [];
+upplant[0] = {
     x : cvs.width,
     y : 0
 };
+var s = [];
+s[0] = {
+    x :cvs.width,
+    y : 300  
+};
+// var dplant = [];
+// dplant[0] = {
+//     x : cvs.width,
+//     y : cvs.height - 300         
+// };
 
+// function render(){
 
+// };
+// setInterval(function(){
+
+// }, 1000/s);
 /*
 / draw images
 */
-function draw(){
+function draw_up(){
     //background
     sf.drawImage(bg,0,0); 
 
     //plant
-    for(var i = 0; i < plant.length; i++){
+    for(var i = 0; i < upplant.length; i++){
         //test draw plants
         constant = up_plant.height + gap;
         sf.drawImage(
-            up_plant, plant[i].x, plant[i].y
+            up_plant, upplant[i].x, upplant[i].y
             );
-        sf.drawImage(
-            down_plant, plant[i].x, plant[i].y + constant
-            );
-        plant[i].x--;
-        if (plant[i].x == 150){
-            // console.log(up2.height, up2.width); //170-52
-            // console.log(down2.height, down2.width);  //184-173
-            // console.log(down_plant.height, down_plant.width) //184-
-            console.log(down_plant.height )
-            plant.push(
+        // sf.drawImage(
+        //     down_plant, plant[i].x, plant[i].y + constant
+        //     );
+        upplant[i].x--;
+        
+        //var timer = setTimeout(callShark, 3000);
+        // function callShark(){
+        //     sf.drawImage(
+        //         shark, Math.floor(Math.random()*100), Math.floor(Math.random()*)
+        //         );
+        // }
+
+        if (upplant[i].x == 200){
+            console.log(up_plant.width, up_plant.height)
+            upplant.push(
                 {
                     x : cvs.width,
                     y : Math.floor(Math.random()*up_plant.height) - up_plant.height
                 }
             );
         }
-   
-        // if(up_pos[i].x == 190){
-        //     //push new plant with random function
-        //     up_pos.push({
-        //         x : cvs.width, 
-        //         //- Math.floor(Math.random() *  10),
-        //         y : Math.floor(Math.random() * 100) - Math.floor(Math.random() * 50)
-        //     });
-        // }
-        // if(down_pos[i].x == 200){
-        //     down_barr.push({
-        //         x : Math.floor(Math.random() * 600),
-        //         y : Math.floor(Math.random() * 200) + Math.floor(Math.random() * 50)
-        //     });
-        // }
-
+    
         /*
         * Game over rule
         */
-       
-        //hit the plant
-        if (CX + creature.width >= plant[i].x 
-            && CX <= plant[i].x + up_plant.width
-            && (CY <= plant[i].y + up_plant.height || CY + creature.height >= cvs.height - plant[i].y)
+        if ((CX + creature.width >= upplant[i].x 
+            && CX <= upplant[i].x + up_plant.width
+            && CY <= upplant[i].y + up_plant.height)
+            || CY + creature.height > cvs.height - 8 //touch the ground
             ){
-                
+                windown.location.href = 'google.com';
                 location.reload();
-        }
-
-        // if (CX + creature.width >= plant[i].x 
-        //     && CX < plant[i].x + down_plant.width 
-        //     && CY + creature.height >= down_plant.height){
-        //         console.log("touch the downplant at", down_plant.x, down_plant.y);
-        //     location.reload();
-        // }
-        
-        //touch the ground
-        if(CY + creature.height > cvs.height - 8 ){
-            location.reload();
         }
         
         /*
         * record the score
         */
-        if(plant[i].x == 10){
+        if(upplant[i].x == 15){
             score++;
             //scor.play();
         }
-        
-        
     }
 
     /*
@@ -195,11 +185,60 @@ function draw(){
     sf.fillText("Score : "+score,10,cvs.height-20);
     
     //callback func
-    requestAnimationFrame(draw);
-    
+    requestAnimationFrame(draw_up);
 }
+draw_up();
+ 
+// function draw_down(){
+//     sf.drawImage(bg,0,0); 
+//     for(var i = 0; i < dplant.length; i++){
+//         sf.drawImage(
+//             down_plant, dplant[i].x, dplant[i].y + gap
+//             );
+//         dplant[i].x--;
+//         if (dplant[i].x == 110){
+//             //console.log(up_plant.width, up_plant.height)
+//             console.log(down_plant.x, down_plant.y)
+//             dplant.push(
+//                 {
+//                     x : cvs.width + 20,
+//                     y : 150 + Math.floor(Math.random()* 100)
+//                 }
+//             );
+//         }
 
-draw();
+//         if (CX + creature.width >= dplant[i].x 
+//             && CX < dplant[i].x + down_plant.width 
+//             && CY > sf.height - down_plant.height){
+//                 console.log("touch the downplant at", down_plant.x, down_plant.y);
+//             location.reload();
+//         }
+        
+//         if(CY + creature.height > cvs.height - 8 ){
+//             location.reload();
+//         }
+
+//         if(dplant[i].x == 150){
+//             score++;
+//             //scor.play();
+//         }
+//     }
+//     /*
+//     /  draw creature images
+//     */
+//    sf.drawImage(creature,CX,CY);
+    
+//    CY += gravity;
+   
+//    sf.fillStyle = "#000";
+//    sf.font = "32px Verdana";
+//    sf.fillText("Score : "+score,10,cvs.height-20);
+   
+//    //callback func
+//    requestAnimationFrame(draw_down);
+// }
+
+
 
 
 
