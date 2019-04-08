d977481fa989964725466557b453fd3e5a466091
lu.yin2046 <lu.yin2046@gmail.com>
2019-04-08 09:51:52 -0500

    Purchaing page with creature information
    First, download by npm: $ npm i -S react-awesome-modal

---
+++ client/src/components/market/Purchasing.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import Modal from 'react-awesome-modal';
+//npm i -S react-awesome-modal
+
+
+class Purchasing extends Component {
+    state = {name: this.props.name,
+            birthTime:this.props.birthTime,
+            generation:this.props.generation,
+            price:this.props.price,
+            visible: false
+    }
+
+    openModal() {
+        this.setState({
+            visible : true
+        });
+    }
+
+    closeModal() {
+        this.setState({
+            visible : false
+        });
+    }
+
+    render() { 
+        return (  
+            <section>
+                <h1>Creature information</h1>
+                <input type="button" value="Open" onClick={() => this.openModal()} />
+                <Modal visible={this.state.visible} width="400" height="300" effect="fadeInUp" onClickAway={() => this.closeModal()}>
+                    <div>
+                        <p>Creature Contents</p>
+                        <h1>Creature Name: {this.state.name}</h1>
+                        <h2>Birthday: {this.state.birthTime}</h2>
+                        <h3>Generation: {this.state.generation}</h3>
+                        <h4>Now : ${this.state.price}</h4>>
+                        <a href="javascript:void(0);" onClick={() => this.closeModal()}>Close</a>
+                    </div>
+                </Modal>
+            </section>
+        );
+    }
+}
+ 
+export default Purchasing;
